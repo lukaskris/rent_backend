0206fd18b54732ba9d67eb4680954f35d854557f
lukaskris12 <lukaskris12@gmail.com>
2020-04-21 21:57:42 +0700

    update setting postgre

--- rent_backend/settings.py
+++ rent_backend/settings.py
@@ -112,8 +112,12 @@ WSGI_APPLICATION = 'rent_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sewano',
+        'USER': 'lukas_kris',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -149,6 +153,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+SITE_ID = 2
 
 
 # Static files (CSS, JavaScript, Images)
